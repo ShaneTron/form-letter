d46ec726ff97db35c0366a99410f8daa17037cb5
Malgorzata Haniszewska and Shane Robinson <shane@>
2017-10-18 11:34:46 -0700

    fix classes to repair js problems

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#name").submit(function(event) {
     var person = $("input#personName").val();
-    $(".personName").text(person);
+    $(".Name").text(person);
     $("#letter").show();
     event.preventDefault();
   });
